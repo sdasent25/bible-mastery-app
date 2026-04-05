b244274041743962dde6f7c16f6ffdeb84c85956
Sam Dasent <contact@ecommercechica.com>
2026-04-05 12:40:42 -0400

    Fix layout: sidebar width + main content rendering

--- components/layout/AppLayout.tsx
+++ components/layout/AppLayout.tsx
@@ -5,10 +5,14 @@ import Sidebar from "@/components/layout/Sidebar"
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen bg-[#020617] text-white">
-      <Sidebar />
+      <div className="w-64 shrink-0">
+        <Sidebar />
+      </div>
 
-      <div className="flex-1">
-        {children}
+      <div className="flex-1 overflow-y-auto">
+        <div className="p-6">
+          {children}
+        </div>
       </div>
     </div>
   )
